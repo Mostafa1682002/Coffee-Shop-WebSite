9af0806e2ecf4f0d3846344bcdc3c673f8723c1f
Mostafa1682002 <ma9856603@gmail.com>
2022-08-19 03:00:08 +0200

    Last Uploaded

--- JS/main.js
+++ JS/main.js
@@ -53,7 +53,6 @@ allLinks.forEach(function(e){
     }
 })
 
-
 let cart=document.querySelector(".cart");
 let sectionCart=document.querySelector(".cart-section");
 
@@ -65,23 +64,17 @@ cart.onclick=function(){
     //remove class Visible from Links
     links.classList.remove("visible");
 }
-
-
 //End Header
-
-
 
 //Footer
 let date=new Date();
 let year=document.querySelector(".year");
-
 year.innerHTML=date.getFullYear();
 
 
 
-
+//Button Scroll
 let buttonUp=document.querySelector(".btn");
-
 window.onscroll=function(){
     if(window.scrollY >= 600){
         buttonUp.classList.add("dis");
@@ -89,8 +82,21 @@ window.onscroll=function(){
         buttonUp.classList.remove("dis");
     }
 }
-
-
 buttonUp.onclick=function(){
     window.scrollTo(0,0)
+}
+
+
+//if onclick on document remove all class from search ,links ,cart
+document.body.children[1].onclick=function(){
+    if(document.body.children[0].children[0].children[1].classList.contains("visible")){
+        //remove class Visible from Links
+        links.classList.remove("visible");
+    }else if(document.body.children[0].children[0].children[2].children[3].classList.contains("show")){
+        //remove class show from input
+        search_input.classList.remove("show");
+    }else if(document.body.children[0].children[0].children[3].classList.contains("found")){
+        //remove class found from Cart
+        sectionCart.classList.remove("found");
+    }
 }
